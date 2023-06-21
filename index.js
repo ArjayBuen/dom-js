b315c1e5f05d5847f87aca62439ac609d349a10a
ArjayBuen <arjaybuen10@gmail.com>
2023-06-21 20:33:49 +0800

    learning dom

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 // alert("Hello World");
 // document.querySelector("h1").innerHTML="Good bye"
+//document.lastElementChild.querySelector("ul").lastElementChild.innerHTML="I Got It";
+// document.getElementsByTagName("li")[2].style.color="green"; 
+// document.getElementsByClassName("btn")[0].style.color="green";
+// document.getElementById("list");
