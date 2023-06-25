22214c7ac24204f5978674bfdbdf56686843c745
ArjayBuen <arjaybuen10@gmail.com>
2023-06-25 21:02:37 +0800

    understanding dom

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // alert("Hello World");
 // document.querySelector("h1").innerHTML="Good bye"
+// document.firstElementChild;=html
+// document.firstElementChild.firstElementChild;=head
+// document.firstElementChild.lastElementChild;=body
 //document.lastElementChild.querySelector("ul").lastElementChild.innerHTML="I Got It";
 // document.getElementsByTagName("li")[2].style.color="green"; 
 // document.getElementsByClassName("btn")[0].style.color="green";
